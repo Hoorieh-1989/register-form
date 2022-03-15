e718f4e5d1eecb213ae48c4e1d10543e4cefa7b1
Hourieh <hoorieh.23@gmail.com>
2022-03-15 21:54:05 +0100

    changes done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
+import Login from './login/Login';
+import  Register from './register/Register';
+import{ BrowserRouter as Router ,
+Route,
+Link,
+Routes} from 'react-router-dom';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <ul>
+        
+        <li><Link to ="/">Home</Link></li>
+        <li><Link to="/Register">Register</Link></li>       
+        </ul>
+<Routes>
+        <Route path="/" element={<Login/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+        </Routes>
+
+
+   
+      </Router>//symantic ellement instead of div
   );
 }
 
 export default App;
+
+
---
+++ src/register/Register.js
@@ -0,0 +1,140 @@
+import { useRef, useState, useEffect } from "react";
+import "./register.css";
+
+export default function Form() {
+  // States for registration
+  const [name, setName] = useState("");
+  const [emails, setEmails] = useState("");
+  const [password, setPassword] = useState("");
+
+  // States for checking the errors
+  const [submitted, setSubmitted] = useState(false);
+  const [errors, setErrors] = useState("");
+
+  const [validName, setValidName] = useState(false);
+  const [validPwd, setValidPwd] = useState(false);
+  const [validEmail, setValidEmail] = useState(false);
+
+  const userRef = useRef();
+  const errRef = useRef();
+
+  useEffect(() => {
+    setValidName(false);
+    setValidEmail(false);
+    setValidPwd(false);
+    setErrors("");
+  }, [name, password, emails]);
+
+  function validateForm() {
+    let hasErrors = false;
+    if (!name) {
+      setValidName(true);
+      setErrors("Enter valid Username");
+      hasErrors = true;
+      return hasErrors;
+    }
+    if (!password) {
+      setValidPwd(true);
+      setErrors("Enter valid password");
+      hasErrors = true;
+      return hasErrors;
+    }
+    if (!emails) {
+      setValidEmail(true);
+      setErrors("Enter valid Email");
+      hasErrors = true;
+      return hasErrors;
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    //Validate user input
+      const hasError = validateForm();
+      if (!hasError) {
+        
+          setSubmitted(true)
+      }
+
+  };
+
+  return (
+    <>
+      {submitted ? (
+        <section>
+          <h1>User {name} successfully registered!!</h1>
+          <br />
+          <p>
+            <a href="/">Go to Home</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={setErrors ? "errors" : "offscreen"}
+            aria-live="assertive"
+          ></p>
+          <h2>Register </h2>
+
+          <form onSubmit={handleSubmit}>
+            <br />
+            {validName && (
+              <p className="errmsg" aria-live="assertive">
+                {errors}
+              </p>
+            )}
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setName(e.target.value)}
+              value={name} // clear input
+            />
+
+            <br />
+            {validPwd && (
+              <p className="errmsg" aria-live="assertive">
+                {errors}
+              </p>
+            )}
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password} // clear input
+            />
+
+            <br />
+            {validEmail && (
+              <p className="errmsg" aria-live="assertive">
+                {errors}
+              </p>
+            )}
+            <label htmlFor="email">Email:</label>
+            <input
+              type="text"
+              id="emails"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setEmails(e.target.value)}
+              value={emails} // clear input
+            />
+
+            <button>Register</button>
+
+           
+             
+          
+          </form>
+        </section>
+      )}
+    </>
+  );
+}
